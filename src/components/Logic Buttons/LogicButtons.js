4f26f43299abd26f47135469d322cff417fd67ea
RodneyMac <rodneydavidmaciel@gmail.com>
2022-09-05 23:20:06 -0300

    Logic Buttons component, style added

--- src/components/Logic Buttons/LogicButtons.js
+++ src/components/Logic Buttons/LogicButtons.js
@@ -5,8 +5,17 @@ const LogicButtons = () => {
   return (
     <>
       <Nav/>
-      <div className='mt-4 d-flex justify-content-center'>
-        <h2 className='text-white'>Logic Buttons</h2>
+      <div className='mt-4 mb-4 d-flex justify-content-center'>
+        <div className=''>
+        <h3 className='text-success mt-2 text-center'>Logic Buttons</h3>
+          <div className='mb-3 mt-3'>
+            <input type="number" className='form-control' name="number" autoFocus/>
+          </div>
+          <div className='mt-4 text-center'>
+            <button className='btn btn-outline-info mx-2'>1</button>
+            <button className='btn btn-outline-info mx-2'>2</button>
+          </div>
+        </div>
       </div>
     </>
   )
