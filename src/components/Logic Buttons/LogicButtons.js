105ddb90408c095bef6f079bd4246f9ac77754cb
RodneyMac <rodneydavidmaciel@gmail.com>
2022-09-06 08:53:31 -0300

    Logic Buttons component, finished

--- src/components/Logic Buttons/LogicButtons.js
+++ src/components/Logic Buttons/LogicButtons.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../Nav/Nav';
 
 const LogicButtons = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleChange = (e) => {
+    setQuantity({
+      ...quantity,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleRequire = (e) => {
+    e.preventDefault();
+    if(!e.target.classList.contains("active")) {
+      e.target.classList.add("active");
+      setQuantity(quantity + 1);
+    } else {
+      e.target.classList.remove("active");
+      setQuantity(quantity - 1);
+    }
+  }
+
   return (
     <>
       <Nav/>
@@ -9,11 +29,11 @@ const LogicButtons = () => {
         <div className=''>
         <h3 className='text-success mt-2 text-center'>Logic Buttons</h3>
           <div className='mb-3 mt-3'>
-            <input type="number" className='form-control' name="number" autoFocus/>
+            <input type="text" className='form-control' name="number" onChange={handleChange} value={quantity} autoFocus/>
           </div>
           <div className='mt-4 text-center'>
-            <button className='btn btn-outline-info mx-2'>1</button>
-            <button className='btn btn-outline-info mx-2'>2</button>
+            <button onClick={handleRequire} className='btn btn-outline-info mx-2'>1</button>
+            <button onClick={handleRequire} className='btn btn-outline-info mx-2'>2</button>
           </div>
         </div>
       </div>
