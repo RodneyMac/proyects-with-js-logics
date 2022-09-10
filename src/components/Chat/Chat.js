c42006673df43cec1c8946ab5d33bbfef98f6a21
RodneyMac <rodneydavidmaciel@gmail.com>
2022-09-10 12:49:28 -0300

    functions added for crud component, chat style modificate

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -43,7 +43,7 @@ const Chat = () => {
     <>
       <Nav/>
       <div className='mt-4 mb-4 d-flex flex-column justify-content-center align-items-center'>
-        <div className='border rounded px-4 w-50'>
+        <div className='border rounded px-4 w-auto'>
           <h3 className='text-info mt-2 text-center'>Chat 1</h3>
           <div className='mb-3 mt-3'>
             <input type="text" className='form-control' placeholder="Write a message here" name="chat1" onChange={handleChangeChat1} value={chat1.name} autoFocus/>
@@ -52,7 +52,7 @@ const Chat = () => {
             <button className='btn btn-outline-primary' onClick={handleChat1}>Send</button>
           </div>
         </div>
-        <div className='border rounded px-4 w-50 mt-4'>
+        <div className='border rounded px-4 w-auto mt-4'>
           <h3 className='text-warning mt-2 text-center'>Chat 2</h3>
           <div className='mb-3 mt-3'>
             <input type="text" className='form-control' placeholder="Write a message here" name="chat2" onChange={handleChangeChat2} value={chat2.name}/>
@@ -62,7 +62,7 @@ const Chat = () => {
           </div>
         </div>
         <h3 className='text-primary text-center mt-4'>Message History</h3>
-        <div className='w-50 mt-3'>
+        <div className='w-auto mt-3'>
           <ol className='text-center'>
             {chatList.map((item) => {
               if(item.chat1) {
--- src/components/Crud/Crud.js
+++ src/components/Crud/Crud.js
@@ -9,6 +9,7 @@ const Crud = () => {
 
   const [addUser, setAddUser] = useState({});
   const [userList, setUserList] = useState([]);
+  const [findUser, setFindUser] = useState({name: "", age: ""});
 
   const handleAddUserChange = (e) => {
     setAddUser({
@@ -24,23 +25,49 @@ const Crud = () => {
     setAddUser({});
     console.log("New User:", newUser);
   }
+
+  const handleFindUserChange = (e) => {
+    setFindUser({
+      ...findUser,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleFindUser = (e) => {
+    e.preventDefault();
+    let find = userList.find((item) => item.name === findUser.name);
+    setFindUser(find);
+  }
+
+  const handleEditUser = (e) => {
+    e.preventDefault();
+    setUserList((state) => state.map((item) => item.id === findUser.id ? findUser : item));
+    console.log("User Edited: ", findUser);
+  }
+
+  const handleDeleteUser = (e) => {
+    e.preventDefault();
+    let deleteUser = userList.filter((item) => item.id !== findUser.id);
+    setUserList(deleteUser);
+    console.log("User deleted: ", deleteUser);
+  }
   
   return (
     <>
       <Nav/>
       <div className='mt-4 mb-4 d-flex justify-content-center'>
-        <form className='border rounded px-4 w-50'>
+        <form className='border rounded px-4 w-auto'>
           <h3 className='text-warning mt-2 text-center'>Add User</h3>
           <div className='mb-3 mt-3'>
-            <input type="text" className='form-control' placeholder="Add name" name="name" onChange={handleAddUserChange} value={addUser.name} autoFocus/>
+            <input type="text" className='form-control' placeholder="Enter name" name="name" onChange={handleAddUserChange} autoFocus/>
           </div>
           <div className='mb-3 mt-3'>
-            <input type="number" className='form-control' placeholder="Add age" name="age" onChange={handleAddUserChange} value={addUser.age}/>
+            <input type="number" className='form-control' placeholder="Enter age" name="age" onChange={handleAddUserChange}/>
           </div>
           <div className='mb-3 text-center'>
-            <button onClick={handleAddUser} className='btn btn-primary'>Send</button>
+            <button className='btn btn-primary' onClick={handleAddUser}>Send</button>
           </div>
-          <ol className='text-center d-flex justify-content-center'>
+          <ol className='d-flex flex-column text-center text-info'>
             {userList.map((item) => (
               <li key={item.id}><b className='text-info'>Id:</b><b className='text-warning mx-2'>{item.id}</b><b className='text-info'>Name:</b><b className='text-warning mx-2'>{item.name}</b><b className='text-info'>Age:</b><b className='text-warning mx-2'>{item.age}</b></li>
             ))}
@@ -49,21 +76,22 @@ const Crud = () => {
           <h3 className='text-warning mt-2 text-center'>Find User</h3>
           <ol></ol>
           <div className='mb-3 mt-3'>
-            <input type="text" className='form-control' placeholder="Find name" name="name"/>
+            <input type="text" className='form-control' placeholder="Search by name to edit or delete" name="name" onChange={handleFindUserChange} value={findUser.name}/>
           </div>
           <div className='mb-3 text-center'>
-            <button className='btn btn-primary'>Find</button>
+            <button className='btn btn-primary' onClick={handleFindUser}>Find</button>
           </div>
           <hr className='text-white'/>
           <h3 className='text-warning mt-2 text-center'>Edit User</h3>
           <div className='mb-3 mt-3'>
-            <input type="text" className='form-control' placeholder="Edit name" name="name"/>
+            <input type="text" className='form-control' placeholder="Name" name="name" onChange={handleFindUserChange} value={findUser.name}/>
           </div>
           <div className='mb-3 mt-3'>
-            <input type="number" className='form-control' placeholder="Edit age" name="age"/>
+            <input type="number" className='form-control' placeholder="Age" name="age" onChange={handleFindUserChange} value={findUser.age}/>
           </div>
-          <div className='mb-3 text-center'>
-            <button className='btn btn-primary'>Edit</button>
+          <div className='mb-3 d-flex justify-content-around'>
+            <button className='btn btn-primary' onClick={handleEditUser}>Edit</button>
+            <button className='btn btn-danger' onClick={handleDeleteUser}>Delete</button>
           </div>
         </form>
       </div>
